a0ae469aa964b5d8a5453494ec60fd93f40836a2
KausBoss <ktkaustubh4@gmail.com>
2020-08-07 11:54:34 +0530

    7 Aug

--- Class13/API/app.js
+++ Class13/API/app.js
@@ -6,17 +6,25 @@ app.use('/', express.static(__dirname + '/static'));
 
 app.get('/addtodo', (req, res) => {
 	todo.push(req.query.task);
-	// res.send('Task added successfully');
-	res.redirect('/todo');
+	res.send('Success');
+	// res.redirect('/todo')
 });
 
 app.get('/todo', (req, res) => {
 	res.send(todo);
 });
 
-// app.get('/', (req, res) => {
-// 	res.sendFile(__dirname + '/index.html');
-// });
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/app.js',(req,res)=>{
+//     res.send('There! we are again!')
+// })
+
+app.get('/js', (req, res) => {
+	res.send('<script>console.log("Hello")</script>');
+});
 
 app.listen(4444, () => {
 	console.log('Server started at http://localhost:4444');
