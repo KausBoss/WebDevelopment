254ff68ab1da19073da16b12527e990c593b79dc
KausBoss <ktkaustubh4@gmail.com>
2020-08-11 04:30:26 +0530

    11 Aug

---
+++ Class14/TemplatingLanguage/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+app.set('views', 'myfolder');
+
+let users = [];
+
+app.get('/', (req, res) => {
+	res.render('tasks', {
+		title: 'Handlebars',
+		users
+	});
+});
+
+app.post('/hi', (req, res) => {
+	users.push({
+		id: req.body.id || 0,
+		name: req.body.name
+	});
+	res.redirect('/');
+});
+
+app.listen(4444, () => {
+	console.log('Server started at http://localhost:4444');
+});
